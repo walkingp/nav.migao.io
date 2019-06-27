4e0ea5418d8811d1c61e60852dcff276c3efed4e
米高哥 <qc_69799f1c597a48e2b692280097438c7a@git.code.tencent.com>
2019-06-27 21:58:20 +0800

    Add all files

---
+++ precache-manifest.6c2e64d4876356bccd5e4afa176f73a0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "499f239c448f14f2929e8a49aa452acf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ac7bb24354af3cb065ab",
+    "url": "/static/css/main.c178300e.chunk.css"
+  },
+  {
+    "revision": "cb3c35d19248c6e90c9d",
+    "url": "/static/js/2.f6fa752c.chunk.js"
+  },
+  {
+    "revision": "ac7bb24354af3cb065ab",
+    "url": "/static/js/main.39d77882.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
